ba6245e48c4d309208e8fef6566f96b149fa5433
javex <florian.ruechel@inexplicity.de>
2024-06-13 22:23:03 +0930

    Filter non-rotational disks

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,5 +1,5 @@
 use anyhow::{bail, Context, Result};
-use log::{debug, error};
+use log::{debug, error, warn};
 use prometheus::{Encoder, GaugeVec, Opts, Registry, TextEncoder};
 use std::fs::{self, read_dir, DirEntry};
 use std::io::BufWriter;
@@ -99,27 +99,86 @@ impl DiskStatus for Hdparm {
     }
 }
 
+fn filter_map_disk(entry: DirEntry) -> Option<Result<String>> {
+    let entry = entry.path();
+    let rotational = entry.join("queue/rotational");
+    if !rotational.exists() {
+        debug!("Missing path '{}'", rotational.to_string_lossy());
+        return None;
+    }
+
+    let rotational_value = match fs::read_to_string(&rotational) {
+        Err(e) => {
+            error!(
+                "Error reading value from '{}'",
+                rotational.to_string_lossy()
+            );
+            return Some(Err(e.into()));
+        }
+        Ok(v) => v.trim().to_string(),
+    };
+
+    match rotational_value.as_ref() {
+        "0" => {
+            debug!(
+                "'{}' is not a rotational device",
+                rotational.to_string_lossy()
+            );
+            None
+        }
+        "1" => {
+            let entry = match entry.file_name() {
+                None => {
+                    warn!("Path '{}' has trailing slash", entry.to_string_lossy());
+                    return None;
+                }
+                Some(entry) => entry.to_string_lossy(),
+            };
+
+            if !entry.starts_with("sd") {
+                debug!("Path '{}' does not start with 'sd', ignoring...", entry);
+                None
+            } else {
+                Some(Ok(format!("/dev/{entry}")))
+            }
+        }
+        _ => {
+            warn!(
+                "Invalid value for {}: {}",
+                rotational.to_string_lossy(),
+                rotational_value
+            );
+            None
+        }
+    }
+}
+
 fn get_all_disks(sysfs: &Path) -> Result<Vec<String>> {
     let block_dir = sysfs.join("block");
     let dir_entries: Result<Vec<DirEntry>, _> = read_dir(block_dir)
         .context("Failed to list sysfs/block")?
         .collect();
-    let result: Vec<String> = dir_entries?
+    let result: Result<Vec<String>> = dir_entries?
         .into_iter()
-        .map(|d| String::from(d.file_name().to_string_lossy()))
-        .filter(|d| d.starts_with("sd"))
-        .map(|d| format!("/dev/{d}"))
+        .filter_map(filter_map_disk)
         .collect();
-    Ok(result)
+    result
 }
 
 #[cfg(test)]
 mod test {
-    use std::fs::{self, create_dir};
+    use std::fs::{self, create_dir_all};
 
     use tempfile::TempDir;
 
     use super::*;
+
+    fn init() {
+        let _ = env_logger::builder()
+            .filter_level(log::LevelFilter::Debug)
+            .is_test(true)
+            .try_init();
+    }
 
     struct FakeHdparm {}
     impl DiskStatus for FakeHdparm {
@@ -131,10 +190,43 @@ mod test {
     #[test]
     fn it_works() {
         // prepare test
+        init();
         let sysfs = TempDir::new().unwrap();
         let block = sysfs.path().join("block");
-        create_dir(&block).unwrap();
-        fs::write(block.join("sda"), "").unwrap();
+        let sda = block.join("sda/queue");
+        create_dir_all(&sda).unwrap();
+        fs::write(sda.join("rotational"), "1").unwrap();
+        let textfile_collector = TempDir::new().unwrap();
+        let disk_status = textfile_collector.path().join("disk_status.prom");
+        let disk_query = FakeHdparm {};
+        let monitor =
+            DiskMonitor::new(sysfs.path().to_path_buf(), disk_status.to_path_buf()).unwrap();
+
+        // run a single cycle
+        monitor.update_metrics(&disk_query).unwrap();
+
+        // compare results
+        let disk_metrics = fs::read_to_string(&disk_status).unwrap();
+        let expected = String::from(
+            "# HELP disk_status Status of the disk (1=active, 0=standby)
+# TYPE disk_status gauge
+disk_status{disk=\"/dev/sda\"} 0\n",
+        );
+        assert_eq!(disk_metrics, expected);
+    }
+
+    #[test]
+    fn test_rotational() {
+        // prepare test
+        init();
+        let sysfs = TempDir::new().unwrap();
+        let block = sysfs.path().join("block");
+        let sda = block.join("sda/queue");
+        create_dir_all(&sda).unwrap();
+        fs::write(sda.join("rotational"), "1\n").unwrap();
+        let sdb = block.join("sdb/queue");
+        create_dir_all(&sdb).unwrap();
+        fs::write(sdb.join("rotational"), "0\n").unwrap();
         let textfile_collector = TempDir::new().unwrap();
         let disk_status = textfile_collector.path().join("disk_status.prom");
         let disk_query = FakeHdparm {};
